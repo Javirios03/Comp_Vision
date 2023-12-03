2dc2a265d02ec308632ade6ebd8708acf0b0c39e
Pepe <202111246@alu.comillas.edu>
2023-12-04 00:51:48 +0100

    papeRPIano

--- 5 - Piano/paperpiano.py
+++ 5 - Piano/paperpiano.py
@@ -5,6 +5,7 @@ import os
 import pygame
 import time
 import threading
+from picamera2 import PiCamera2 as PiCamera
 
 pygame.init()
 # Inicializar el módulo mixer
@@ -69,10 +70,34 @@ def piano(frame, playing):
     return piano_notes_played    
  
 
-if __name__ == "__main__":
-    # create_marker(1, 12)
-    # read_markers()
-    # acceder a la camara y pasar la imagen frame a frame a main
+def stream_video_rpi():
+    camera = PiCamera()
+    camera.preview_configuration.main.size = (640, 480)
+    camera.preview_configuration.main.format = "RGB888"
+    camera.preview_configuration.align()
+    camera.video_configuration.controls.FrameRate = 10.0
+
+    camera.configure("preview")
+    camera.start()
+
+    playing = []
+    # Capture frames from the camera
+    while True:
+        # Grab a single frame of video
+        frame = camera.capture_array()
+        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Display the resulting frame
+        playing = piano(frame, playing)
+        cv.imshow('Video', frame)
+        # Wait for Enter key to stop
+        if cv.waitKey(1) == 13:
+            break
+
+    # When everything done, release the capture
+    cv.destroyAllWindows()
+
+
+def stream_video():
     cap = cv.VideoCapture(0)
     playing = []
     while True:
@@ -83,3 +108,12 @@ if __name__ == "__main__":
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
+    cap.release()
+
+
+if __name__ == "__main__":
+    # create_marker(1, 12)
+    # read_markers()
+    # acceder a la camara y pasar la imagen frame a frame a main
+    # stream_video()
+    stream_video_rpi()
