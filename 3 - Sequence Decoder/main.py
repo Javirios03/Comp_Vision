fa0d8214d55410d17b922279f9f6746f9ce873fc
Javier Ríos <202111258@alu.comillas.edu>
2023-12-03 17:17:23 +0100

    Folder for Piano created

--- 3 - Sequence Decoder/main.py
+++ 3 - Sequence Decoder/main.py
@@ -1,7 +1,47 @@
 from detect_shapes import *
 import cv2
 
-if __name__ == '__main__':
+def pattern_rec():
     for i in range(1, 5):
         img = cv2.imread("Patterns/pattern{}.jpg".format(i))
         main(img)
+
+def blob_rec():
+    from math import sqrt
+    from skimage import data
+    from skimage.feature import blob_dog, blob_log, blob_doh
+    from skimage.color import rgb2gray
+
+    import matplotlib.pyplot as plt
+
+    image = cv2.imread('Patterns/Kindle.jpg')
+    #image = data.hubble_deep_field()[0:500, 0:500]
+    image_gray = rgb2gray(image)
+
+    blobs_dog = blob_dog(image_gray, threshold=0.1)
+    blobs_dog[:, 2] = blobs_dog[:, 2] * sqrt(2)
+
+
+    # Implementa aquí tu código para visualizar los blobs calculados usando LoG y DoG
+    fig, ax = plt.subplots(1, 3, figsize=(12, 6))
+    ax[0].imshow(image, cmap='gray')
+    ax[0].set_title('Original image')
+
+    for blob in blobs_dog:
+        y, x, r = blob
+        c = plt.Circle((x, y), r, color='red', linewidth=2, fill=False)
+        ax[2].add_patch(c)
+    ax[2].imshow(image, cmap='gray')
+    ax[2].set_title('Blobs using DoG')
+
+    plt.show()
+
+
+def pattern_rec1():
+    img = cv2.imread("Patterns/Kindle.jpg")
+    main(img)
+
+if __name__ == '__main__':
+    # pattern_rec()
+    # blob_rec()
+    pattern_rec1()
---
+++ 4 - Tracker/detect_shapes.py
@@ -0,0 +1,153 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def get_corners_list(img):
+    # Convert to gray scale
+    gray = np.float32(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+    # gaussian blur
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    # Detect corners. The parameters used are the same as in the previous part
+    block_size = 5
+    ksize = 5
+    k = 0.04
+    corners = cv2.cornerHarris(gray, 2, 3, 0.04)
+    # Dilate to mark the corners (used from the OpenCV documentation) (not important)
+    corners = cv2.dilate(corners, None)
+    # We wont use the same threshold
+    threshold = 0.01
+    corners_coords = np.argwhere(corners > threshold * corners.max())
+    points = []
+    # quitamos los bordes muy cercanos a otros bordes
+    min_dist = 20
+    for point in corners_coords:
+        if len(points) == 0:
+            points.append(point)
+        else:
+            for p in points:
+                if np.linalg.norm(point-p) < min_dist:
+                    break
+            else:
+                points.append(point)
+        
+    return points
+
+def get_contours(img):
+    # Convertir la imagen a escala de grises
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Aplicar un desenfoque gaussiano para reducir el ruido
+    gray = cv2.medianBlur(gray, 5)
+
+    # Detectar los bordes
+    edges = cv2.Canny(gray, 50, 150)
+
+    edges = cv2.dilate(edges, None,iterations=4)
+
+    edges = cv2.GaussianBlur(edges, (5, 5), 0)
+
+    # dibujar los bordes de azul
+    # plt.imshow(edges,cmap = 'gray')
+    # plt.show()
+
+    # Encontrar contornos
+    contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # show edges and contours
+    # for cnt in contours:
+    #     # Obtener rectángulo de contorno
+    #     x, y, w, h = cv2.boundingRect(cnt)
+
+    #     # Dibujar el rectángulo
+    #     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Mostrar la imagen original con los contornos
+    # plt.imshow(img)
+    # plt.show()
+
+    return contours
+
+def get_circles(img):
+    # Convertir la imagen a escala de grises
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Aplicar un desenfoque gaussiano para reducir el ruido
+    gray = cv2.medianBlur(gray, 5)    
+
+    # Usar la transformada de Hough para encontrar círculos
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 200, param1=50, param2=30, minRadius=20, maxRadius=450)
+
+    # Verificar si se encontraron círculos
+    # if circles is not None:
+    #     circles = np.uint16(np.around(circles))
+    #     for i in circles[0, :]:
+    #         # Dibujar el círculo externo
+    #         cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    #         # Dibujar el centro del círculo
+    #         cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+
+    # # Mostrar la imagen con los círculos detectados
+    # plt.imshow(img)
+    # plt.show()
+
+    return circles
+
+def points_in_contours(points, contour):
+    # points: list of points
+    # contour: x,y,w,h
+    # returns: list of points that are inside the contours
+    points_in = []
+    x, y, w, h = cv2.boundingRect(contour)
+    extra = 10
+    for point in points:
+        if x-extra <= point[1] <= x+w+extra and y-extra <= point[0] <= y+h+extra:
+            points_in.append(point)
+    return points_in
+        
+
+def main(img):
+    points = get_corners_list(img)
+    contours = get_contours(img)
+    circles = get_circles(img)
+
+    shapes_names = []
+    contours_to_show = []
+
+    for contour in contours:
+        points_in = points_in_contours(points, contour)
+        if len(points_in) == 0:
+            contours_to_show.append(contour)
+            shapes_names.append('circle')
+        
+        elif len(points_in) == 3:
+            contours_to_show.append(contour)
+            shapes_names.append('triangle')
+        elif len(points_in) == 4:
+            contours_to_show.append(contour)
+            shapes_names.append('square')
+        elif len(points_in) > 4:
+            contours_to_show.append(contour)
+            shapes_names.append('star')
+        else:
+            pass
+        # print(len(points_in))
+
+    # show contours and shapes names
+    for i, contour in enumerate(contours_to_show):
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.putText(img, shapes_names[i], (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+    plt.imshow(img)
+    # show points
+    plt.scatter([p[1] for p in points],[p[0] for p in points],c='r')
+    plt.show()
+    
+
+
+if __name__ == '__main__':
+    path = "patterns/"
+    for i in range(1, 5):
+        img = cv2.imread(path + "pattern" + str(i) + ".jpg")
+        main(img)
